6341a10f4239ad1cd6e05d80b892b3697054293c
shira-barsela <shira.barsela@mail.huji.ac.il>
2025-09-27 16:33:23 +0300

    wierd graph

--- numerical_calculation.py
+++ numerical_calculation.py
@@ -1,5 +1,7 @@
 import numpy as np
 from typing import Callable
+import matplotlib.pyplot as plt
+
 import constants as cons
 import functions as func
 
@@ -19,25 +21,41 @@ def calc_xi(g_func, i: int) -> None:
     if i == 0:
         x_array[0] = cons.X0
         return
+
     sum = 0
     sum += 0.5 * g_func(i, 0) * x_array[0]
 
-    for j in range(1, i+1):
+    for j in range(1, i):
         sum += g_func(i, j) * x_array[j]
+
     sum *= cons.DELTA_T
-    sum += fi
 
-    denominator = 1 - (cons.DELTA_T * g_func(i, i) / 2)
+    denom = 1.0 - 0.5 * cons.DELTA_T * g_func(i, i)
+    x_array[i] = (fi + sum) / denom
 
-    x_array[i] = sum / denominator
-
+def plot_x_array():
+    t = np.arange(cons.TIME_STEPS) * cons.DELTA_T
+    plt.figure(figsize=(8, 4))
+    plt.plot(t, x_array, label="x(t)")
+    plt.xlabel("time")
+    plt.ylabel("x")
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     # define G_function_by_indexes with the current G function
     def g_by_idx(t1: int, t2: int) -> float:
         return func.g_function_by_ind(g_chosen_func, t1, t2)
+
     # fill f array
     fill_f_array(f_array, f_func)
+
     # calc x array
     for i in range(cons.TIME_STEPS):
         calc_xi(g_by_idx, i)
+
+    # plot x array
+    print(x_array)
+    plot_x_array()
